7499aa7b0c03402cdd1e69ef95563f0c5311487d
vagrant <vagrant@precise32.(none)>
2013-08-24 02:39:06 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DvdLibrary::Application.config.secret_key_base = '83c6356c48448271af52273fc15c810889cd3f4f8f595f3fc05d417a4f343a73b163cc25d5dbd614d6c4d476eed1454a4b6695e14082b38d28d0830ebcda39e2'
