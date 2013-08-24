f3c3e189505c21d8a902551c36cfd090163c0cb6
Xynthy <coder@xynthy.com>
2013-08-24 16:41:32 +0000

    Moved generation of dvds into controller

--- app/controllers/dvds_controller.rb
+++ app/controllers/dvds_controller.rb
@@ -1,4 +1,10 @@
+Dvd = Struct.new :location, :title
+
 class DvdsController < ApplicationController
 	def in_library
+		@dvds = [
+			Dvd.new("Case 1 Page 1", "Star Wars Episode IV"),
+			Dvd.new("Case 2 Page 2", "Star Trek")
+		]
 	end
 end
